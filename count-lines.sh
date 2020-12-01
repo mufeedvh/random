2ffcf1197cf55bcca6915e520551de30c1e673a6
Mufeed VH <mufeedvh@gmail.com>
2020-12-01 20:36:20 +0530

    printf bro

--- count-lines.sh
+++ count-lines.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $# -eq 0 ]] ; then
-    echo "yo give me the codebase's file extension bruh\n\nExample: $ ./count-lines.sh php"
+    printf "yo give me the codebase's file extension bruh\n\nExample: $ ./count-lines.sh php"
     exit 0
 fi
 
